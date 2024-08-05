46732c866f9c1c171027308b0c72028abd6d63d2
ibrahim <ibrahimsaad6677@gmail.com>
2024-08-05 21:35:58 +0300

    arrowupdate

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,11 +1,8 @@
-// 2-arrow.js  
-export default class GetNeighborhoodsList {  
-    constructor() {  
-      this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];  
-    }  
+export default function getNeighborhoodsList() {  
+    this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];  
   
-    addNeighborhood = (newNeighborhood) => {  
+    this.addNeighborhood = (newNeighborhood) => {  
       this.sanFranciscoNeighborhoods.push(newNeighborhood);  
       return this.sanFranciscoNeighborhoods;  
     };  
-  }
+}
