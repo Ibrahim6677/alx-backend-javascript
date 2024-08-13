02076130b39a18b2deea6c71d491d962f6175e97
ibrahim <ibrahimsaad6677@gmail.com>
2024-08-13 10:33:57 +0300

    updates

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,5 +1,13 @@
 
-
+/**
+ * Retrieves the sum of ids of a list of students.
+ * @param {{
+*   id: Number,
+*   firstName: String,
+*   location: String
+* }[]} students - The list of students.
+* @returns {Number}
+*/
 
 export default function getStudentIdsSum(students, id) {
     if (students instanceof Array){
